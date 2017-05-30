53d4f9ff7e10716e2d30b0b4d59fe397049ce85b
Kieren Powell <kieren@blinkmobile.com.au>
2017-05-31 09:24:58 +1000

    Fix formatting

--- test/lib/s3-urls.js
+++ test/lib/s3-urls.js
@@ -7,30 +7,45 @@ const boom = require('boom')
 
 test('Should succeed when passed valid input', (t) => {
     var aws = td.replace('aws-sdk').S3
-    td.when(aws.prototype.getSignedUrl(
-                    'getObject',
-                    td.matchers.anything()
-                )
-            ).thenCallback(
-                null,
-                'www.geturl.com'
-            )
-    td.when(aws.prototype.getSignedUrl(
-                    'putObject',
-                    td.matchers.anything()
-                )
-            ).thenCallback(
-                null,
-                'www.puturl.com'
-                )
+    td.when(
+        aws.prototype.getSignedUrl(
+            'getObject',
+            td.matchers.anything()
+        )
+    ).thenCallback(
+        null,
+        'www.geturl.com'
+    )
+
+    td.when(
+        aws.prototype.getSignedUrl(
+            'putObject',
+            td.matchers.anything()
+        )
+    ).thenCallback(
+        null,
+        'www.puturl.com'
+    )
     
     var lib = require('../../lib/s3-urls.js')
 
     process.env.S3_BUCKET = 'testbucket'
     var result = lib.retrieveS3Urls('blobTest')
 
-    td.verify(aws.prototype.getSignedUrl('getObject',td.matchers.anything(),td.matchers.anything()))
-    td.verify(aws.prototype.getSignedUrl('putObject',td.matchers.anything(),td.matchers.anything()))
+    td.verify(
+        aws.prototype.getSignedUrl(
+            'getObject',
+            td.matchers.anything(),
+            td.matchers.anything()
+        )
+    )
+    td.verify(
+        aws.prototype.getSignedUrl(
+            'putObject',
+            td.matchers.anything(),
+            td.matchers.anything()
+        )
+    )
 
     td.reset()
     process.env.S3_BUCKET = ''
@@ -40,22 +55,24 @@ test('Should succeed when passed valid input', (t) => {
 
 test('Should failed when geturl fails', (t) => {
     var aws = td.replace('aws-sdk').S3
-    td.when(aws.prototype.getSignedUrl(
-                    'getObject',
-                    td.matchers.anything()
-                )
-            ).thenCallback(
-                'S3 is down',
-                ''
-            )
-    td.when(aws.prototype.getSignedUrl(
-                    'putObject',
-                    td.matchers.anything()
-                )
-            ).thenCallback(
-                null,
-                'www.puturl.com'
-                )
+    td.when(
+        aws.prototype.getSignedUrl(
+            'getObject',
+            td.matchers.anything()
+        )
+    ).thenCallback(
+        'S3 is down',
+        ''
+    )
+    td.when(
+        aws.prototype.getSignedUrl(
+            'putObject',
+            td.matchers.anything()
+        )
+    ).thenCallback(
+        null,
+        'www.puturl.com'
+    )
     
     var lib = require('../../lib/s3-urls.js')
 
@@ -84,22 +101,24 @@ test('Should failed when geturl fails', (t) => {
 
 test('Should failed when puturl fails', (t) => {
     var aws = td.replace('aws-sdk').S3
-    td.when(aws.prototype.getSignedUrl(
-                    'getObject',
-                    td.matchers.anything()
-                )
-            ).thenCallback(
-                null,
-                'www.geturl.com'
-            )
-    td.when(aws.prototype.getSignedUrl(
-                    'putObject',
-                    td.matchers.anything()
-                )
-            ).thenCallback(
-                'S3 is down',
-                ''
-                )
+    td.when(
+        aws.prototype.getSignedUrl(
+            'getObject',
+            td.matchers.anything()
+        )
+    ).thenCallback(
+        null,
+        'www.geturl.com'
+    )
+    td.when(
+        aws.prototype.getSignedUrl(
+            'putObject',
+            td.matchers.anything()
+        )
+    ).thenCallback(
+        'S3 is down',
+        ''
+    )
     
     var lib = require('../../lib/s3-urls.js')
 
